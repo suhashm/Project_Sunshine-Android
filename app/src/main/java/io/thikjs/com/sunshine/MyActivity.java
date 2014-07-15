f6f30cf8717d446c517ef008218ea2ff280b1b2f
Suhas <suhas.hm109@gmail.com>
2014-07-16 02:54:28 +0530

    Implemented Getting data from WeatherAPI and populating in the ListView

--- app/src/main/java/io/thikjs/com/sunshine/MyActivity.java
+++ app/src/main/java/io/thikjs/com/sunshine/MyActivity.java
@@ -1,32 +1,22 @@
 package io.thikjs.com.sunshine;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.ArrayAdapter;
-import android.widget.FrameLayout;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 
 public class MyActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        Log.d("TestLog", "First debug log in onCreate");
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -51,45 +41,4 @@ public class MyActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-
-            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
-
-            //Dummy Data for ListView of weekly weather forecasts
-            String[] forecastArray = {
-                    "Mon 6/23 - Sunny - 31/17",
-                    "Tue 6/24 - Foggy - 21/8",
-                    "Wed 6/25 - Cloudy - 22/17",
-                    "Thurs 6/26 - Rainy - 18/11",
-                    "Fri 6/27 - Foggy - 21/10",
-                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                    "Sun 6/29 - Sunny - 20/7"
-            };
-            ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
-
-            //Create an Array adapter to bind the above dummy data to the listview
-            ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(
-                    getActivity(),
-                    R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textview,
-                    weekForecast
-            );
-
-            ListView lv = (ListView) rootView.findViewById(R.id.listview_forecast);
-            lv.setAdapter(mForecastAdapter);
-
-
-            return rootView;
-        }
-    }
 }
